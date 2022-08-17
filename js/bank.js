fa60f4b16efdd5d8beb80567570c1162ac323ed6
rakib ahmed <rakibahmed.dev@gmail.com>
2022-08-17 16:06:59 +0600

    project done

--- js/bank.js
+++ js/bank.js
@@ -1,3 +1,7 @@
+/*=======
+ Deposit button 
+ ===========*/
+
 //  step 1: add click event  handler with the Deposti button
 document
   .getElementById("deposit-button")
@@ -29,6 +33,43 @@ document
     // step 8: set the current balance to the Total Balance
     balanceTotalElement.innerText = currentBalanceTotal;
 
-    // step 10: clear the deposit input filed
+    // step 9: clear the deposit input filed
     depositField.value = "";
+  });
+
+/*=======
+Withdraw button 
+ ===========*/
+
+//  step 1: add click event handler with Withdraw button
+document
+  .getElementById("withdraw-button")
+  .addEventListener("click", function (event) {
+    // step 2: stop button reload issue
+    event.preventDefault();
+
+    // step 3: get the input filed value
+    const withdrawFiled = document.getElementById("withdraw-input");
+    const newWithdrawAmountString = withdrawFiled.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    // step 4: get the value from top withdraw section
+    const withdrawTotalElement = document.getElementById("withdraw-total");
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+    // step 5: calculate new withdraw from input and previous withdraw from top
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    // step 6: reduce withdraw input amount from total balance
+    const newBalanceTotalElement = document.getElementById("balance-total");
+    const newBlanceTotalString = newBalanceTotalElement.innerText;
+    const newBalanceTotalConvert = parseFloat(newBlanceTotalString);
+    const newCurrentWithdrawConvert = parseFloat(currentWithdrawTotal);
+    const newBlanceTotal = newBalanceTotalConvert - newCurrentWithdrawConvert;
+    newBalanceTotalElement.innerText = newBlanceTotal;
+
+    // step 7: clear withdraw input value
+    withdrawFiled.value = "";
   });
